8f776b8adf9a382b3e75ba181a59fec2a2c7b5b3
Imran Jami <imranajami@gmail.com>
2024-12-13 16:43:22 -0800

    Update MetadataRenderer.sol

--- src/custom/MetadataRenderer.sol
+++ src/custom/MetadataRenderer.sol
@@ -57,14 +57,14 @@ contract MetadataRenderer {
     }
 
     function setInitialMetadata(uint256 tokenId) external onlyNFTContract {
-        uint8[7] memory defaultValues = [15, 30, 45, 60, 75, 90, 100];
+        uint8[7] memory defaultValues = [1, 2, 3, 0, 0, 0, 0];
         uint8 palette;
         uint256 mod16 = tokenId % 16;
         
-        if (mod16 < 8) palette = 0;      // 8/15 chance = 50%
-        else if (mod16 < 12) palette = 1; // 4/15 chance = 31.25%
-        else if (mod16 < 14) palette = 2; // 2/15 chance = 12.5%
-        else palette = 3;                 // 1/15 chance = 6.25%
+        if (mod16 < 8) palette = 0;      // 8/15 chance = 53.33%
+        else if (mod16 < 12) palette = 1; // 4/15 chance = 26.67%
+        else if (mod16 < 14) palette = 2; // 2/15 chance = 13.33%
+        else if (mod16 < 15) palette = 3; // 1/15 chance = 6.67%
 
         setTokenMetadata(
             tokenId,
@@ -152,7 +152,10 @@ contract MetadataRenderer {
             abi.encodePacked(
                 '"attributes": [',
                 '{"trait_type": "values", "value": "', _generateValueString(values), '"},',
-                '{"trait_type": "palette", "value": "', _getPaletteName(palette), '"}',
+                '{"trait_type": "palette", "value": "', _getPaletteName(palette), '"},',
+                '{"display_type": "number", "trait_type": "current_value", "value": ', 
+                uint256(_getCurrentValue(values)).toString(), 
+                ', "max_value": 100, "min_value": 0}',
                 ']'
             )
         );
@@ -184,5 +187,18 @@ contract MetadataRenderer {
         if (palette == 4) return "chromatic";
         if (palette == 5) return "pastel";
         return "greyscale";
+    }
+
+    function _getCurrentValue(uint8[7] memory values) 
+        internal 
+        pure 
+        returns (uint8) 
+    {
+        for (uint256 i = 6; i >= 0; i--) {
+            if (values[i] > 0) {
+                return values[i];
+            }
+        }
+        return 0; // Return 0 if all values are zero
     }
 }
